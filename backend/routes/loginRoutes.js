44c85e8a81201ac441a1c829e3b98f7e158f645a
Rafael Medeiros <pedro.mape7@gmail.com>
2024-12-01 20:12:58 -0300

    começando o frontend

--- backend/routes/loginRoutes.js
+++ backend/routes/loginRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import { criarConta, listarContas, pegarUmaConta } from "../controllers/loginControllers.js";
 
 export function routes(app) {
     app.use(express.json());
+    app.use(cors())
 
     app.get('/login', listarContas);
     app.get('/login/:email', pegarUmaConta);
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -8,3 +8,20 @@ function mostrarSenha(idSenha) {
         input.type = 'password';
     }
 };
+
+async function acharContas() {
+    try {
+        const response = await fetch('localhost:3000/login');
+        const data = await response.json();
+        window.alert(data)
+    } catch (error) {
+        console.error('erro', error)
+    }
+};
+
+const submit = document.getElementsByClassName('submit');
+
+for (let i = 0; i < submit.length; i++) {
+    const element = submit[i];
+    element.addEventListener('click', acharContas);
+}
