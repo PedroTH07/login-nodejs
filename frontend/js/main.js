bf673558a013e586d41d77010c505cccf217021a
Rafael Medeiros <pedro.mape7@gmail.com>
2024-12-06 21:42:44 -0300

    verificando o login

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -14,11 +14,22 @@ async function acharConta() {
         const inputEmail = document.getElementById('emailGet');
         const emailGet = inputEmail.value;
         if (emailGet === "") {
-            console.log('insira um email');
+            window.alert('insira um email');
         } else {
             const response = await fetch(`http://localhost:3000/login/${emailGet}`);
             const data = await response.json();
-            console.log(data);
+            const inputSenha = document.getElementById('senha3');
+            const senhaGet = inputSenha.value;
+            try {
+                const senhaUser = data.senha;
+
+                if (senhaGet === senhaUser) {
+                    window.alert('login bem sucedido');
+                } else { window.alert('senha incorreta') };
+            } catch (error) {
+                window.alert('email não encontrado');
+            }
+
         }
     } catch (error) {
         console.error('erro', error.message);
