885af83a42a9d39d225f661a2fa4e04e12665fd8
Rafael Medeiros <pedro.mape7@gmail.com>
2024-12-16 12:36:47 -0300

    função para atualizar contas

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -1,7 +1,37 @@
 const userId = localStorage.getItem('userId');
 const btnLogout = document.getElementById('btnConta');
 const btnAtualizar = document.getElementById('btnAtualizacao');
-const btnSair = document.getElementById('btnSair')
+const btnSair = document.getElementById('btnSair');
+
+async function atualizarConta() {
+    const novoEmail = document.getElementById('emailAtulizacao').value;
+    const novaSenha = document.getElementById('senhaAtulizacao').value;
+    const newData = {};
+    if (novoEmail) {
+        newData.email = novoEmail
+    };
+    if (novaSenha) {
+        newData.senha = novaSenha
+    };
+
+    try {
+        const response = await fetch(`https://login-nodejs-api.onrender.com/update/${userId}`,{
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: newData
+        });
+        if (response.status === 200) {
+            const data = await response.json();
+            return data;
+        } else {
+            window.alert('algo deu errado');
+            return false;
+        }
+    } catch (error) {
+        console.error('erro', error);
+        window.alert('algo deu errado');
+    };
+}
 
 if (userId) {
     async function fetchData() {
@@ -38,6 +68,28 @@ if (userId) {
         painel.style.display = 'none';
         event.stopPropagation();
     });
+
+    document.getElementById('ocultar').addEventListener('click', (event) => {
+        const input = document.getElementById('senhaAtualizacao');
+        if (input.type === 'password') {
+            input.type = 'text';
+        } else {
+            input.type = 'password';
+        };
+        event.stopPropagation();
+    });
+
+    async function concluirAtulizacao(event) {
+        const data = await atualizarConta();
+        if (data.acknowledged) {
+            window.location.reload();
+        } else {
+            window.alert('algo deu errado');
+        };
+        event.stopPropagation();
+    };
+
+    document.getElementById('btnConfirmar').addEventListener('click', concluirAtulizacao)
 
 } else {
     btnAtualizar.style.display = 'none'
