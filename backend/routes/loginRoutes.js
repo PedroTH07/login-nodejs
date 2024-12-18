af9bdae68b5de342ff1a67da2cfa74c2bf4e3deb
Rafael Medeiros <pedro.mape7@gmail.com>
2024-12-18 15:00:07 -0300

    update cors

--- backend/routes/loginRoutes.js
+++ backend/routes/loginRoutes.js
@@ -2,13 +2,13 @@ import express from "express";
 import cors from "cors";
 import { atualizarConta, criarConta, pegarContaId, pegarUmaConta } from "../controllers/loginControllers.js";
 
-const allowedOrigins = ['https://login-nodejs-brown.vercel.app', 'https://login-nodejs-brown.vercel.app/html/homepage.html']
+const allowedOrigins = ['https://login-nodejs-brown.vercel.app']
 
 export function routes(app) {
     app.use(express.json());
     app.use(cors({
         origin: allowedOrigins,
-        methods: ['GET', 'POST', 'PUT', 'DELETE']
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
     }));
 
     app.get('/getInfo/:id', pegarContaId);
